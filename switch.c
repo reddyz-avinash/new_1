1cce8f8994efd5bbe8f67fff20d36c46862eb5a9
Vijay-sathya <vijay.s1@s.amity.edu>
2025-03-28 12:12:22 +0530

    added one case

---
+++ switch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+     printf("Enter a number (1-3): ");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("Btech cse\n");
+            break;
