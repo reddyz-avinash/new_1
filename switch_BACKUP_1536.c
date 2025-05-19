c2cfef5a3e6f3d4c46450666e4cd871e19841608
Vijay-sathya <vijay.s1@s.amity.edu>
2025-05-19 20:57:24 +0530

    added a new line

---
+++ switch_BACKUP_1536.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+     printf("Enter a number (1-3): ");
+    scanf("%d", &choice);
+    switch (choice) {
+        case 1:
+            printf("Btech cse\n");
+	    printf("not");
+	    printf("nothing");
+            break;
+	    case 2:
+            printf("Btech aiml\n");
+<<<<<<< HEAD
+	    printf("not");
+=======
+	    printf(not);
+	    printf("nothing");
+>>>>>>> ts2
+            break;
+	     case 3:
+            printf("Btech mec\n");
+            break;
+	     case 4:
+	    printf("pharmc\n");
+	    break;
+	     case 5:
+            printf("btech civil\n");
+            break;
+	     case 5:
+	    printf("pharm\n");
+         break:
+default:
+            printf("Invalid choice! Please enter 1, 2, or 3.\n");
+    }
+
+    return 0;
+}
